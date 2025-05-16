b418a4890b5bb19e573ec0915ca678a6fbfac609
Khanh Nguyen <khanh.nguyenv2@gmail.com>
2025-05-16 11:38:53 -0700

    api key call fixed

--- hw3-app/backend/app.py
+++ hw3-app/backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, session
+from flask import Flask, jsonify, redirect, url_for, session
 from authlib.integrations.flask_client import OAuth
 from authlib.common.security import generate_token
 import os
@@ -24,6 +24,10 @@ oauth.register(
     device_authorization_endpoint="http://dex:5556/device/code",
     client_kwargs={'scope': 'openid email profile'}
 )
+
+@app.route('/api/key')
+def get_key():
+    return jsonify({'apiKey': os.getenv('NYT_API_KEY')})
 
 @app.route('/')
 def home():
